121610cea617eb88115ddd7a0a8da9beb24292b4
rivedroz23 <river.droz@gmail.com>
2019-08-20 16:06:45 -0700

    styled buttons

--- server.js
+++ server.js
@@ -94,10 +94,7 @@ app.put("/shelters/:id", (req, res) => {
 
 app.delete("/shelters/:id", (req, res) => {
     Shelter.findByIdAndRemove(req.params.id, (err, shelter) => {
-        // As always, handle any potential errors:
         if (err) return res.status(500).send(err);
-        // We'll create a simple object to send back with a message and the id of the document that was removed
-        // You can really do this however you want, though.
         const response = {
             message: "Shelter successfully deleted",
             id: shelter._id
